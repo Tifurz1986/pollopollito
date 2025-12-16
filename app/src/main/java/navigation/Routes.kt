cf3841ff78f5a227a0fe6350eb2003da440df5bd
tifurz <cajromu@gmail.com>
2025-12-16 02:47:43 +0100

    borrado navigator

--- app/src/main/java/navigation/Routes.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pollopollito.navigation
-
-object Routes {
-    const val LOGIN = "login"
-    const val MAIN = "main"
-    const val TODAY = "today"
-    const val HISTORY = "history"
-    const val ADD = "add"
-}
